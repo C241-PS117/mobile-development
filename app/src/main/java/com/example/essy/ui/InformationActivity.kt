f7447eb3ec7f18680548bc217b6f4c999f7eb9fa
Krisna Choiril Andika <99785580+khrisnandika@users.noreply.github.com>
2024-06-09 22:02:55 +0700

    add custom toolbar

--- app/src/main/java/com/example/essy/ui/InformationActivity.kt
+++ app/src/main/java/com/example/essy/ui/InformationActivity.kt
@@ -5,16 +5,26 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
 import com.example.essy.R
 import com.example.essy.databinding.ActivityInformationBinding
 
 class InformationActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivityInformationBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityInformationBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_information)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_information)
 
+        initUi()
+
+    }
+
+    private fun initUi(){
+        binding.customToolbar.btnBack.setOnClickListener {
+            finish()
+        }
+        binding.customToolbar.txtTitle.text = "Informasi Aplikasi"
     }
 }
