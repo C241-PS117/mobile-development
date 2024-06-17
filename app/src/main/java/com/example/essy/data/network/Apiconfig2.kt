3b0e8f2926b2eb50ad65d42f52b6504451b3adc8
IndraPras <indrapras2002@gmail.com>
2024-06-17 23:55:27 +0700

    add API Config for model

---
+++ app/src/main/java/com/example/essy/data/network/Apiconfig2.kt
@@ -0,0 +1,30 @@
+package com.example.essy.data.network
+
+import com.example.essy.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class Apiconfig2 {
+
+    companion object {
+        fun getApiService(): ApiService {
+            val loggingInterceptor = if(BuildConfig.DEBUG) {
+                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+            } else {
+                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.NONE)
+            }
+            val client = OkHttpClient.Builder()
+                .addInterceptor(loggingInterceptor)
+                .build()
+            val retrofit = Retrofit.Builder()
+                .baseUrl("https://backend2-jvegqzvkyq-et.a.run.app/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build()
+            return retrofit.create(ApiService::class.java)
+        }
+    }
+
+}
